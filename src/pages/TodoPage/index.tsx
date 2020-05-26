49c9754be2b3ac6eecdf89973aaa429b6b2aec3c
qqdtf98 <qqdtf98@gmail.com>
2020-05-26 15:53:26 +0900

    feat: get user data

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,18 +1,28 @@
 import './index.scss'
 
 import React from 'react'
+import { RouteComponentProps } from 'react-router'
 
-// import TodoList from '../../componets/TodoList/index.js'
+// import TodoList from '../../../componets/TodoList/index.js'
 
-export const UserContent = React.createContext('')
+export const UserContent = React.createContext({
+  userId: '-1',
+})
 
-function TodoPage() {
+type UrlUserId = {
+  userId: string
+}
+
+function TodoPage(urlData: RouteComponentProps) {
+  console.log(urlData)
+  const data = urlData.match.params as UrlUserId
+
   return (
     <>
-      {/* <UserContent.Provider value={{ userId: urlData.match.params.userId }}>
+      <UserContent.Provider value={{ userId: data.userId }}>
         <div>TodoPage</div>
-        <TodoList id="todo-page" />
-      </UserContent.Provider> */}
+        {/* <TodoList id="todo-page" /> */}
+      </UserContent.Provider>
     </>
   )
 }
